2b43dbd5d7ff94cdd51b5b3842507197ca4bab4f
skisenge <skisengese@gmail.com>
2024-08-19 00:39:16 +0300

    test: initial commit

---
+++ iteration/repeat_test.go
@@ -0,0 +1,11 @@
+package iteration
+
+import "testing"
+
+func TestRepeat(t *testing.T) {
+	repeated := Repeat("a")
+	expected := "aaaaa"
+	if repeated != expected {
+		t.Errorf("expected %q, but repeated %q", repeated, expected)
+	}
+}
