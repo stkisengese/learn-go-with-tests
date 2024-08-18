15c599e3b9c83ed651345c8388ffd6abe6a102a4
skisenge <skisengese@gmail.com>
2024-08-18 22:43:34 +0300

    refactor: use of contants

--- Hello-world/hello.go
+++ Hello-world/hello.go
@@ -2,14 +2,18 @@ package main
 
 import "fmt"
 
-const engHelloPrefix = "Hello, "
+const (
+	spanish            = "Spanish"
+	engHelloPrefix     = "Hello, "
+	spanishHelloPrefix = "Hola, "
+)
 
 func hello(name, language string) string {
 	if name == "" {
 		return "Hello, World"
 	}
-	if language == "Spanish" {
-		return "Hola, " + name
+	if language == spanish {
+		return spanishHelloPrefix + name
 	}
 	return engHelloPrefix + name
 }
