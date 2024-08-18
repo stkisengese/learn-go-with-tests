888124e5b225b0efca32a66177872aec73be792c
skisenge <skisengese@gmail.com>
2024-08-19 00:42:48 +0300

    refactor: initialized constant variable

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,11 @@
 package iteration
 
+const repeatCount = 5
+
 func Repeat(character string) string {
 	var repeated string
-	for i := 0; i < 5; i++ {
-        repeated += character
-    }
+	for i := 0; i < repeatCount; i++ {
+		repeated += character
+	}
 	return repeated
 }
-
