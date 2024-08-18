0900c8877fb6e081370c3d9bcafefb9c5305a26a
skisenge <skisengese@gmail.com>
2024-08-19 00:50:54 +0300

    test: add benchmark

--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -9,3 +9,9 @@ func TestRepeat(t *testing.T) {
 		t.Errorf("expected %q, but repeated %q", repeated, expected)
 	}
 }
+
+func BenchmarkRepeat(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		Repeat("a")
+	}
+}
