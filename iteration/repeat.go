16cb47aea740cb157fd22c988a9bb3e25788d2e2
skisenge <skisengese@gmail.com>
2024-08-19 00:59:49 +0300

    feat: add repeat test case

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -1,10 +1,8 @@
 package iteration
 
-const repeatCount = 5
-
-func Repeat(character string) string {
+func Repeat(character string, repeat int) string {
 	var repeated string
-	for i := 0; i < repeatCount; i++ {
+	for i := 0; i < repeat; i++ {
 		repeated += character
 	}
 	return repeated
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,15 +3,24 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a")
-	expected := "aaaaa"
-	if repeated != expected {
-		t.Errorf("expected %q, but repeated %q", repeated, expected)
-	}
+	t.Run("Valid repeat", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+        expected := "aaaaa"
+        if repeated != expected {
+            t.Errorf("expected %q, but repeated %q", repeated, expected)
+        }
+	})
+	t.Run("Caller to specify repeat", func(t *testing.T) {
+		repeated := Repeat("a", 3)
+        expected := "aaa"
+        if repeated != expected {
+            t.Errorf("expected %q, but repeated %q", repeated, expected)
+        }
+	})
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
-		Repeat("a")
+		Repeat("a", 3)
 	}
 }
