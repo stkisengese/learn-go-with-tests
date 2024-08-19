83f0c3371bd4e44d0453c7dfc3bf42fcfe9df426
skisenge <skisengese@gmail.com>
2024-08-19 21:27:50 +0300

    feat: add SumAll() function

--- arrays-slices/array.go
+++ arrays-slices/array.go
@@ -2,8 +2,17 @@ package arraysslices
 
 func Sum(arr []int) int {
 	sum := 0
-    for _, num := range arr {
-        sum += num
-    }
-    return sum
+	for _, num := range arr {
+		sum += num
+	}
+	return sum
+}
+
+func SumAll(numbersToSum ...[]int) []int {
+	sums := make([]int, len(numbersToSum))
+
+	for i, numbers := range numbersToSum {
+		sums[i] = Sum(numbers)
+	}
+	return sums
 }
--- arrays-slices/array_test.go
+++ arrays-slices/array_test.go
@@ -1,6 +1,9 @@
 package arraysslices
 
-import "testing"
+import (
+	"reflect"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("test of 5 numbers", func(t *testing.T) {
@@ -11,6 +14,17 @@ func TestSum(t *testing.T) {
 
 		if got != want {
 			t.Errorf("got %d, want %d given, %v", got, want, numbers)
+		}
+	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("test of multiple slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("got %d, want %d.", got, want)
 		}
 	})
 }
