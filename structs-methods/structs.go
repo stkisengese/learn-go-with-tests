699c4b7b5dc546342ca85eb4dc7e8d02042cc809
skisenge <skisengese@gmail.com>
2024-08-21 00:37:51 +0300

    feat: add methods

--- structs-methods/structs.go
+++ structs-methods/structs.go
@@ -1,9 +1,24 @@
 package structsmethods
 
-func Perimeter(length, width float64) float64 {
-	return 2 * (length + width)
+import "math"
+
+type Rectangle struct {
+	Length float64
+	Height float64
 }
 
-func Area(length, width float64) float64 {
-	return length * width
+type Circle struct {
+	Radius float64
+}
+
+func Perimeter(r Rectangle) float64 {
+	return 2 * (r.Length + r.Height)
+}
+
+func (r Rectangle) Area() float64 {
+	return r.Length * r.Height
+}
+
+func (c Circle) Area() float64 {
+	return math.Pi * c.Radius * c.Radius
 }
