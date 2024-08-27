93e5cd9dccbf9914dba7fd5a180d2c920755d849
skisenge <skisengese@gmail.com>
2024-08-27 19:53:53 +0300

    feat: add populating maps

--- maps/dictionary_test.go
+++ maps/dictionary_test.go
@@ -2,13 +2,36 @@ package main
 
 import "testing"
 
-func TestSearch(t *testing.T) {
-	dictionary := map[string]string{"test": "this is just a test"}
+func TestAdd(t *testing.T) {
+	dictionary := Dictionary{}
+	dictionary.Add("test", "this is just a test")
 
-	got := Search(dictionary, "test")
 	want := "this is just a test"
+	got, err := dictionary.Search("test")
+	if err != nil {
+		t.Fatal("should find added word:", err)
+	}
 
 	assertStrings(t, got, want)
+}
+
+func TestSearch(t *testing.T) {
+	dictionary := Dictionary{"test": "this is just a test"}
+
+	t.Run("known word", func(t *testing.T) {
+		got, _ := dictionary.Search("test")
+		want := "this is just a test"
+
+		assertStrings(t, got, want)
+	})
+
+	t.Run("unknown word", func(t *testing.T) {
+		_, got := dictionary.Search("unknown")
+		if got == nil {
+			t.Fatal("expected to get an error.")
+		}
+		assertError(t, got, ErrWordNotFound)
+	})
 }
 
 func assertStrings(t testing.TB, got, want string) {
@@ -16,5 +39,13 @@ func assertStrings(t testing.TB, got, want string) {
 
 	if got != want {
 		t.Errorf("got %q want %q", got, want)
+	}
+}
+
+func assertError(t testing.TB, got, want error) {
+	t.Helper()
+
+	if got != want {
+		t.Errorf("got error %q want %q", got, want)
 	}
 }
